bee0a9219eb842b5926f0ec8e64cf1aa5c813091
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2021-12-06 17:57:04 +0100

    made some pickups

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Ball : MonoBehaviour
 {
     // Movement
     private float torque = 1f;
-    private float jumpForce = 6f;
+    private float jumpForce = 7f;
     private float jumpForceMultiplier = 1f;
     private float maxVelocity = 30f;
     private float moveValue = 0f;
@@ -18,14 +19,16 @@ public class Ball : MonoBehaviour
     // Also movement kinda
     private bool isGrounded = false;
     private int jumpsSinceGrounded = 0;
+    private bool rollInput = false;
     
     // Abilities
-    private bool hasDash = true;
-    //private bool hasDoubleDash = false;
-    private bool hasDoubleJump = true;
+    private bool hasDash = false;
+    private bool hasDoubleJump = false;
     private bool hasWallJump = false;
     private bool hasInfiniteJump = false;
-    private bool canBuild = false;
+    //private bool canBuild = false;
+    //private bool hasDoubleDash = false;
+
     
     // // Weapons
     // private bool hasBomb = false;
@@ -45,11 +48,16 @@ public class Ball : MonoBehaviour
         {
             Stop();
         }
+        else
+        {
+            Roll();
+        }
     }
 
     private void FixedUpdate()
     {
         Roll();
+        //Debug.Log(rollInput);
     }
 
     private void OnCollisionEnter(Collision other)
@@ -59,6 +67,8 @@ public class Ball : MonoBehaviour
             jumpsSinceGrounded = 0;
             isGrounded = true;
         }
+
+        
     }
 
     private void OnCollisionExit(Collision other)
@@ -69,10 +79,34 @@ public class Ball : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DoubleJumpPickUp"))
+        {
+            hasDoubleJump = true;
+            Destroy(other.gameObject);
+            Debug.Log("DoubleJump " + hasDoubleJump);
+        }
+        
+        if (other.gameObject.CompareTag("InfiniteJumpPickUp"))
+        {
+            hasInfiniteJump = true;
+            Destroy(other.gameObject);
+            Debug.Log("InfiniteJump " + hasInfiniteJump);
+        }
+        
+        if (other.gameObject.CompareTag("WallJumpPickUp"))
+        {
+            hasWallJump = true;
+            Destroy(other.gameObject);
+            Debug.Log("WallJump " + hasWallJump);
+        }
+    }
+
     bool IsTouchingWall()
     {
-        if (Physics.Raycast(transform.position, Vector3.left, 0.6f) ||
-            Physics.Raycast(transform.position, Vector3.right, 0.6f))
+        if (Physics.Raycast(transform.position, Vector3.left, 0.8f) ||
+            Physics.Raycast(transform.position, Vector3.right, 0.8f))
         {
             return true;
         }
@@ -93,7 +127,7 @@ public class Ball : MonoBehaviour
             rigidBody.angularVelocity.magnitude > -maxVelocity &&
             !isStopping)
         {
-            rigidBody.AddTorque(Vector3.back * torque * moveValue, ForceMode.Impulse);
+            rigidBody.AddTorque(Vector3.back * torque * moveValue, ForceMode.VelocityChange);
         }
     }
     
@@ -121,6 +155,7 @@ public class Ball : MonoBehaviour
     {
         isStopping = false;
         moveValue = value.Get<float>();
+        //rollInput = value.isPressed;
     }
 
     void OnStop()
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    private Ball player;
+    
+    
+ 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
